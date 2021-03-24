5206da8372a72e31369a83de37740092bbd7b919
Yusuf6256 <78488257+Yusuf6256@users.noreply.github.com>
2021-03-24 20:08:20 +0530

    Add files via upload

---
+++ ball.js
@@ -0,0 +1,19 @@
+class Ball{
+    constructor(x, y, r) {
+        var options = {
+            'restitution':0.8,
+            'friction':1.0,
+            'density':1.0
+        }
+        this.body = Bodies.circle(x, y, r, options);
+        this.r = r;
+        World.add(world, this.body);
+      }
+      display(){
+        push();
+        translate(this.body.position.x, this.body.position.y);
+        ellipseMode(RADIUS);
+        ellipse(0, 0, this.r);
+        pop();
+      }
+}
